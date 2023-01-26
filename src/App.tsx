32fea6b912a6383c51a94ce13bfb6045123ecdeb
rodionhatskevich <rodionhatskevich@gmail.com>
2023-01-26 22:59:46 +0100

    add reqest

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const ContactList = lazy(() => import('./pages/contact-list/ContactList'));
@@ -7,11 +7,16 @@ const ContactList = lazy(() => import('./pages/contact-list/ContactList'));
 function App() {
   return (
     <div className='app'>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path='/' element={<ContactList />} />
-        </Switch>
-      </Suspense>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ContactList />
+            </Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/pages/contact-list/ContactList.tsx
+++ src/pages/contact-list/ContactList.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+import { IUserData } from '../../interfaces/interfaces';
+import { getContactList } from '../../services/api';
+
 function ContactList() {
-  return <div className='contact-list' />;
+  const [data, setData] = useState<IUserData[]>([]);
+
+  useEffect(() => {
+    getContactList().then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  return <div className='contact-list'></div>;
 }
 
 export default ContactList;
